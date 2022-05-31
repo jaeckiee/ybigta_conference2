fc950f71f4443a92524080bb5a68bf491e1884f8
jaeckiee <jaechan11@gmail.com>
2022-05-31 18:55:46 +0900

    DB settings

---
+++ modules/db.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root', // 이전에 입력했던 비밀번호
+    database: 'test', // 이전에 입력했던 데이터베이스 명
+    multipleStatements: true,
+    connectionLimit: 1000,
+});
+
+exports.query = query => new Promise((resolve, reject) => {
+    connection.getConnection((err, connection) => {
+        if (err) {
+            return reject(err);
+        }
+
+        return connection.query(query, (err2, rows) => {
+            connection.release();
+            if (err2) {
+                return reject(err2);
+            }
+            return resolve(rows);
+        })
+    });
+});
